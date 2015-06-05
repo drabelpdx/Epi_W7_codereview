8d658a380c99281bb43b39d24eb8da31e16f229f
DavidAbel <drabelpdx@gmail.com>
2015-06-05 09:50:14 -0700

    Adds quantity and returns one pizza for order.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-function PizzaOrder(pizzaSize, ingredients, pizzaPrice){
+function Customer(customer){
+  this.customer = customer
+  this.PizzaOrders = []
+}
+
+function PizzaOrder(quantity, pizzaSize, ingredients, pizzaPrice){
+  this.quantity = quantity
   this.pizzaSize = pizzaSize
   this.ingredients = ingredients
   this.pizzaPrice = 0
-
 }
+
+// PizzaOrder.prototype.eachPizza = function() {
+//   return this.quantity + ": Size: " + this.pizzaSize + " Ingredients: " + this.ingredients;
+// }
 
 PizzaOrder.prototype.pizzaCost = function() {
 var baseCost = 0
@@ -20,12 +29,11 @@ var price = 0
   if (this.pizzaSize === "large") {
     baseCost = 9;
   }
+
   var myString = this.ingredients.valueOf();
   var myArray = myString.split(" ")
   var toppings = myArray.length;
 
   price = baseCost + (toppings * .50);
-
   return this.pizzaPrice = price;
-
 }
