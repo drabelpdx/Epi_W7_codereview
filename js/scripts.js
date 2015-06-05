411e4f2f6a77229f295251f2e093ae4e8f6c4b16
DavidAbel <drabelpdx@gmail.com>
2015-06-05 09:17:29 -0700

    Adds price prototype and returns price of one pizza to order.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
-var ingredients = []
-
-function PizzaOrder(pizzaSize, ingredients){
+function PizzaOrder(pizzaSize, ingredients, pizzaPrice){
   this.pizzaSize = pizzaSize
   this.ingredients = ingredients
+  this.pizzaPrice = 0
+
+}
+
+PizzaOrder.prototype.pizzaCost = function() {
+var baseCost = 0
+var price = 0
+
+  if (this.pizzaSize === "small") {
+    baseCost = 5;
+  }
+
+  if (this.pizzaSize === "medium") {
+    baseCost = 7;
+  }
+
+  if (this.pizzaSize === "large") {
+    baseCost = 9;
+  }
+  var myString = this.ingredients.valueOf();
+  var myArray = myString.split(" ")
+  var toppings = myArray.length;
+
+  price = baseCost + (toppings * .50);
+
+  return this.pizzaPrice = price;
 
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,17 @@
 describe('PizzaOrder', function(){
   it("returns the correct size ordered", function(){
-  var testOrder = new PizzaOrder("large");
+    var testOrder = new PizzaOrder("large");
     expect(testOrder.pizzaSize).to.equal("large");
   });
 
   it("return the correct ingredients ordered", function(){
-  var testOrder = new PizzaOrder("large",["olives","mushrooms","sausage"]);
+    var testOrder = new PizzaOrder("large",["olives","mushrooms","sausage"]);
     expect(testOrder.ingredients).to.eql(["olives","mushrooms","sausage"]);
   });
 
-})
+  it("return the correct price for pizza ordered", function(){
+    var testOrder = new PizzaOrder("large", "olives, mushrooms, sausage");
+    testOrder.pizzaCost();
+    expect(testOrder.pizzaPrice).to.equal(10.50);
+  });
+});
