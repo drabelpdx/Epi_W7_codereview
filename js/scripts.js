d4f29182c71804ea3f9ba32517e231e3f0a46f3c
DavidAbel <drabelpdx@gmail.com>
2015-06-05 12:43:56 -0700

    Web page functions and returns the coorect details and price for a single pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 function Customer(customer){
-  this.customer = customer
-  this.PizzaOrders = []
+  this.customer = customer;
+  this.PizzaOrders = [];
 }
 
-function PizzaOrder(quantity, pizzaSize, ingredients, pizzaPrice){
-  this.quantity = quantity
-  this.pizzaSize = pizzaSize
-  this.ingredients = ingredients
-  this.pizzaPrice = 0
+function PizzaOrder(pizzaSize, ingredients, pizzaPrice){
+//  this.quantity = quantity;
+  this.pizzaSize = pizzaSize;
+  this.ingredients = ingredients;
+  this.pizzaPrice = 0;
 }
 
 PizzaOrder.prototype.eachPizza = function() {
   return this.quantity + ": Size: " + this.pizzaSize + " Ingredients: " + this.ingredients;
-}
+};
 
 PizzaOrder.prototype.pizzaCost = function() {
-  var baseCost = 0
-  var price = 0
+  var baseCost = 0;
+  var price = 0;
 
   if (this.pizzaSize === "small") {
     baseCost = 5;
@@ -29,10 +29,51 @@ PizzaOrder.prototype.pizzaCost = function() {
   if (this.pizzaSize === "large") {
     baseCost = 9;
   }
-  //var myString = this.ingredients.valueOf();
-  //var myArray = myString.split(" ")
+//  var myString = this.ingredients.valueOf();
+//  var myArray = myString.split(" ")
   var toppings = this.ingredients.length;
 
-  price = baseCost + (toppings * .50);
+  price = baseCost + (toppings * 0.50);
+  price = parseFloat(price).toFixed(2)
+
   return this.pizzaPrice = price;
-}
+};
+
+
+$(document).ready(function(){
+
+  $("form#pizzaorder").submit(function(event){
+    event.preventDefault();
+
+    var customer = $("input#customer").val();
+    var pizzaSize = $("input[name=size]:checked").val();
+    //var ingredients = $("input[name=ingredients]:checked").serialize();
+    var ingredients = $('input:checkbox[name="ingredients"]:checked').map(function () {
+    return this.value;
+}).get();
+
+    var newCustomer = new Customer(customer);
+
+    var newOrder = new PizzaOrder(pizzaSize,ingredients);
+    newOrder.pizzaPrice = newOrder.pizzaCost();
+
+      $(".customer").text(newCustomer.customer);
+      $(".size").text(newOrder.pizzaSize);
+      $(".ingredients").text(newOrder.ingredients);
+      $(".cost").text("$" + newOrder.pizzaPrice);
+  });
+});
+
+  //   $("#show-ticket").show();
+  //   $("div.form").hide();
+  //   $("button").hide();
+  //
+
+  //
+  // $( ".listing" ).change(function() {
+  //   $(".showtime").show();
+  // });
+  //
+  // $( ".showtime" ).change(function() {
+  //   $(".type").show();
+  // });
